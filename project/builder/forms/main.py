2537d9196406cdd4784bd3775c1cfa72b3628318
Georgi Kostadinov <grgkostadinov@gmail.com>
2012-08-18 22:07:41 +0300

    Improve adding a page.

--- project/builder/forms/main.py
+++ project/builder/forms/main.py
@@ -1,8 +1,10 @@
 from django import forms
 
 from builder.models import *
+from project.views import BaseView
 
 class PageCreateForm(forms.Form):
+	site_url = forms.URLField(label='Your site\'s URL', initial='http://', required=True)
 	site_name = forms.CharField(required=True)
 	site_slogan = forms.CharField(required=True)
 
@@ -13,14 +15,6 @@ class PageCreateForm(forms.Form):
 		# Get the creator
 		creator = request.user
 
-		# Create a page element
-		page_element = PageElement(content='This is a test element.', position=1)
-		page_element.save()
-
-		# Create a page element
-		page_setting = PageSetting(name='email', value='This is the email.')
-		page_setting.save()
-
 		# And a template
 		template = PageTemplate()
 		template.save()
@@ -29,8 +23,21 @@ class PageCreateForm(forms.Form):
 		obj_page.creator = creator
 		obj_page.template = template
 		obj_page.save()
-		
+
+		# Create a page element
+		page_element = PageElement(content=self.cleaned_data.get('site_name'), position=1)
+		page_element.save()
 		obj_page.elements.add(page_element)
+
+		# Create a another page element
+		page_element = PageElement(content=self.cleaned_data.get('site_slogan'), position=2)
+		page_element.save()
+		obj_page.elements.add(page_element)
+
+		# Create a page element
+		confirmation_email = BaseView().template_to_string(request, template_name='confirmation_email.html')
+		page_setting = PageSetting(name='email', value=confirmation_email)
+		page_setting.save()
 		obj_page.settings.add(page_setting)
 
 		return obj_page
