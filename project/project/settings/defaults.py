be136805b55b9c216eac283e850a6597333df08c
Georgi Kostadinov <grgkostadinov@gmail.com>
2012-08-18 16:14:19 +0300

    Improve defaults settings.

--- project/project/settings/defaults.py
+++ project/project/settings/defaults.py
@@ -3,6 +3,7 @@
 import os
 
 PROJECT_ROOT = os.path.join(os.path.abspath(os.path.dirname(__file__)), '..')
+MANAGE_FILE_ROOT = os.path.join(PROJECT_ROOT, '..')
 
 DATABASES = {
     'default': {
@@ -40,7 +41,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = os.path.join(PROJECT_ROOT, '../media/')
+MEDIA_ROOT = os.path.join(MANAGE_FILE_ROOT, 'media/')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -51,7 +52,7 @@ MEDIA_URL = 'media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(PROJECT_ROOT, '../static/')
+STATIC_ROOT = os.path.join(MANAGE_FILE_ROOT, 'static/')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -104,7 +105,8 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 TEMPLATE_DIRS = (
     os.path.join(PROJECT_ROOT, 'templates'),
-    os.path.join(PROJECT_ROOT, 'templates/includes')
+    os.path.join(PROJECT_ROOT, 'templates/includes'),
+    os.path.join(MANAGE_FILE_ROOT, 'builder/templates')
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
