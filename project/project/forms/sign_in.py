3d0a3c6f8978107169f8cc862077ffc77519a879
Ivan Penchev <ivanvpenchev@gmail.com>
2012-08-19 06:48:21 +0300

    Fixed password field

--- project/project/forms/sign_in.py
+++ project/project/forms/sign_in.py
@@ -2,4 +2,4 @@ from django import forms
 
 class SignInForm(forms.Form):
     email = forms.EmailField(required=True)
-    password = forms.CharField(required=True)
+    password = forms.CharField(required=True, widget=forms.PasswordInput())
