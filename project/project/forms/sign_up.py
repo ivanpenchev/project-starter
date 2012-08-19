84c9d78263139eba8fe733846efa96350fa8e33b
Ivan Penchev <ivanvpenchev@gmail.com>
2012-08-19 08:23:44 +0300

    Verify unique email on signup

--- project/project/forms/sign_up.py
+++ project/project/forms/sign_up.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth.models import User
 
 class SignUpForm(forms.Form):
     """
@@ -6,13 +7,27 @@ class SignUpForm(forms.Form):
         given his first and last name and his email and preferred password.
     """
     error_messages = {
-        'password_confirm' : "Password and confirmation didn't match."
+        'email_already_used' : 'The entered email address is already signed up.',
+        'password_confirm' : "Password confirmation didn't match."
     }
     fname = forms.CharField(label="First name", required=True)
     lname = forms.CharField(label="Last name", required=True)
     email = forms.EmailField(required=True)
-    password = forms.CharField(required=True, weidget=forms.PasswordInput())
+    password = forms.CharField(required=True, widget=forms.PasswordInput())
     confirm_password = forms.CharField(required=True, widget=forms.PasswordInput())
+
+    def clean_email(self):
+        """
+            Check if the entered email address isn't already in the database
+            and if it is - raise form ValidationError.
+        """
+        email = self.cleaned_data['email']
+        user_with_this_email = User.objects.filter(email=email).all()
+        if user_with_this_email:
+            raise forms.ValidationError(
+                self.error_messages['email_already_used']
+            )
+        return email
 
     def clean_confirm_password(self):
         """
